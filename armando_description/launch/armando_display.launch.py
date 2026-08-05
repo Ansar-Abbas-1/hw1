46ed353692478baad628e45e642051a5fe4b15aa
Ansar Abbas <ansarabbas615@gmail.com>
2026-08-05 16:33:50 +0000

    Q1(a): Added Armando RViz display launch file

---
+++ armando_description/launch/armando_display.launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+
+def generate_launch_description():
+
+    package_path = get_package_share_directory("armando_description")
+
+    urdf_path = os.path.join(
+        package_path,
+        "urdf",
+        "arm.urdf"
+    )
+
+    with open(urdf_path, "r") as file:
+        robot_description = file.read()
+
+    return LaunchDescription([
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="screen",
+            parameters=[
+                {"robot_description": robot_description}
+            ]
+        ),
+
+        Node(
+            package="joint_state_publisher",
+            executable="joint_state_publisher",
+            output="screen"
+        ),
+
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            output="screen"
+        )
+
+    ])
